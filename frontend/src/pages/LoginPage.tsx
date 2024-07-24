5e08a853f7fa1bb76881f7b002eda73289b783c6
jonathanhu7 <jonathanhu237@gmail.com>
2024-07-24 14:35:44 +0800

    feat(frontend): 完善登录表单

--- frontend/src/pages/LoginPage.tsx
+++ frontend/src/pages/LoginPage.tsx
@@ -11,33 +11,42 @@ import {
 import { FaRegUserCircle } from "react-icons/fa";
 import { CiLock } from "react-icons/ci";
 import logo from "../assets/fastapi-logo.svg";
+import { useForm } from "react-hook-form";
 
 const LoginPage = () => {
+  const { register, handleSubmit } = useForm();
+
   return (
     <Center h="100vh">
-      <Stack>
-        <Image src={logo} maxW="40" mb="8" mx="auto" />
-        <form>
-          <Stack my="4" spacing="6">
+      <Stack w="40vh">
+        <Image src={logo} maxW={60} mb={2} mx="auto" />
+        <form onSubmit={handleSubmit((data) => console.log(data))}>
+          <Stack my={4} spacing={6}>
             <InputGroup>
               <InputLeftElement pointerEvents="none">
                 <FaRegUserCircle color="gray" />
               </InputLeftElement>
-              <Input name="email" type="email" size="md" placeholder="NetID" />
-              <InputRightAddon>@mail2.sysu.edu.cn</InputRightAddon>
+              <Input
+                {...register("NetID")}
+                type="text"
+                size="md"
+                placeholder="NetID"
+                className="form-control"
+              />
             </InputGroup>
             <InputGroup>
               <InputLeftElement pointerEvents="none">
                 <CiLock />
               </InputLeftElement>
               <Input
-                name="password"
+                {...register("password")}
                 type="password"
                 size="md"
                 placeholder="密码"
+                className="form-control"
               />
             </InputGroup>
-            <Button colorScheme="teal" size="md">
+            <Button colorScheme="teal" size="md" type="submit">
               登录
             </Button>
           </Stack>
